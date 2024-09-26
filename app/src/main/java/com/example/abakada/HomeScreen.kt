51e1ca90dc764bbc0571a45f7d354c7751289365
RAIN-20 <carlngaosi.crn@gmail.com>
2024-09-26 22:48:57 +0800

    working about us activity

--- app/src/main/java/com/example/abakada/HomeScreen.kt
+++ app/src/main/java/com/example/abakada/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.example.abakada
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,33 @@ class HomeScreen : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
+        val storyBooksImage: ImageView = findViewById(R.id.image_story_books)
+        val modulesImage: ImageView = findViewById(R.id.image_modules)
+        val quizzesImage: ImageView = findViewById(R.id.image_quizzes)
+        val aboutUsImage: ImageView = findViewById(R.id.image_about_us)
+
+//        storyBooksImage.setOnClickListener {
+//            val intent = Intent(this@HomeScreen, StoryBooksActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        modulesImage.setOnClickListener {
+//            val intent = Intent(this@HomeScreenActivity, ModulesActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        quizzesImage.setOnClickListener {
+//            val intent = Intent(this@HomeScreenActivity, QuizzesActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        aboutUsImage.setOnClickListener {
+            val intent = Intent(this@HomeScreen, AboutUs::class.java)
+            startActivity(intent)
+        }
+
     }
 }
