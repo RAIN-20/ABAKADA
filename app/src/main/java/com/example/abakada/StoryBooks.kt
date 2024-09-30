2cd8a35b77284b6d58420e90e839be6fdbf67d9c
RAIN-20 <carlngaosi.crn@gmail.com>
2024-09-30 22:19:51 +0800

    add two cats and the monkey, and the lion and the mouse

--- app/src/main/java/com/example/abakada/StoryBooks.kt
+++ app/src/main/java/com/example/abakada/StoryBooks.kt
@@ -1,11 +1,33 @@
 package com.example.abakada
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class StoryBooks : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_story_books)
+
+
+        val twoCats: Button = findViewById(R.id.twoCatsBtn)
+        val theLion: Button = findViewById(R.id. theLionBtn)
+
+
+        twoCats.setOnClickListener {
+            val intent = Intent(this@StoryBooks, twoCatsStory::class.java)
+            startActivity(intent)
+        }
+
+        theLion.setOnClickListener {
+            val intent = Intent(this@StoryBooks, lionAndTheMouse::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/abakada/twoCatsStory.kt
@@ -0,0 +1,13 @@
+package com.example.abakada
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class twoCatsStory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_two_cats_story)
+
+
+    }
+}
